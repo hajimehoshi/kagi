ad2a5a185f2bdcec5a321ca824f1692dd6af7ec3
Hajime Hoshi <hajimehoshi@hajimehoshi.com>
2014-04-18 02:17:43 +0900

    Add Filters

--- main.go
+++ main.go
@@ -6,24 +6,105 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Filter interface {
+	Apply(str string) string
+}
+
+func ParseFilter(line string) Filter {
+	fields := strings.Fields(line)
+	if len(fields) < 2 {
+		return nil
+	}
+	name := fields[1]
+	if !strings.HasPrefix(fields[1], "@") {
+		return nil
+	}
+	name = name[1:]
+	args := fields[2:]
+	var filter Filter
+	switch name {
+	case "replace":
+		if len(args) != 2 {
+			return nil
+		}
+		filter = &ReplaceFilter{
+			From: args[0],
+			To:   args[1],
+		}
+	case "substring":
+		if len(args) < 1 || 2 < len(args) {
+			return nil
+		}
+		start := 0
+		length := -1
+		s, err := strconv.Atoi(args[0])
+		if err == nil {
+			start = s
+		}
+		if 2 <= len(args) {
+			l, err := strconv.Atoi(args[1])
+			if err == nil {
+				length = l
+			}
+		}
+		filter = &SubstringFilter{
+			Start:  start,
+			Length: length,
+		}
+	}
+	return filter
+}
+
+type ReplaceFilter struct {
+	From string
+	To   string
+}
+
+func (f *ReplaceFilter) Apply(str string) string {
+	return strings.Replace(str, f.From, f.To, -1)
+}
+
+type SubstringFilter struct {
+	Start  int
+	Length int
+}
+
+func (f *SubstringFilter) Apply(str string) string {
+	if 0 <= f.Length {
+		return str[f.Start:(f.Length-f.Start)]
+	} else {
+		return str[f.Start:]
+	}
+}
+
+type Site struct {
+	Name    string
+	Filters []Filter
+}
 
 func showUsage() {
 	fmt.Fprintf(os.Stderr, "Usage: %s SITES_FILE MASTER_PASS_FILE\n", os.Args[0])
 }
 
 func trim(str string) string {
-	return strings.Trim(str, " \t\n")	
+	return strings.Trim(str, " \t\r\n")
 }
 
-func createPassword(site, masterPass string) string {
-	str := fmt.Sprintf("%s:%s", site, masterPass)
+func (s *Site) Password(masterPass string) string {
+	str := fmt.Sprintf("%s:%s", s.Name, masterPass)
 	bytePass := sha512.Sum512([]byte(str))
-	return base64.StdEncoding.EncodeToString(bytePass[:])[0:32]
+	pass := base64.StdEncoding.EncodeToString(bytePass[:])[0:32]
+	for _, filter := range s.Filters {
+		pass = filter.Apply(pass)
+	}
+	return pass
 }
 
-func loadSites(filename string) []string {
+func loadSites(filename string) []*Site {
 	file, err := os.Open(filename)
 	if err != nil {
 		panic(err)
@@ -33,11 +114,24 @@ func loadSites(filename string) []string {
 	if err != nil {
 		panic(err)
 	}
-	provisionalSites := strings.Split(string(fileContent), "\n")
-	sites := []string{}
-	for _, site := range provisionalSites {
-		site := trim(site)
-		if site != "" && site[0] != '#' {
+	lines := strings.Split(string(fileContent), "\n")
+	sites := []*Site{}
+	latestFilters := []Filter{}
+	for _, line := range lines {
+		line := trim(line)
+		switch {
+		case line == "":
+			latestFilters = []Filter{}
+		case line[0] == '#':
+			filter := ParseFilter(line)
+			if filter != nil {
+				latestFilters = append(latestFilters, filter)
+			}
+		default:
+			site := &Site{
+				Name:    line,
+				Filters: latestFilters,
+			}
 			sites = append(sites, site)
 		}
 	}
@@ -72,7 +166,7 @@ func loadMasterPassword(filename string) string {
 	return trim(string(fileContent))
 }
 
-var sites []string
+var sites []*Site
 var masterPassword string
 
 func init() {
@@ -87,15 +181,15 @@ func init() {
 func main() {
 	longestSiteLen := 0
 	for _, site := range sites {
-		siteLen := len(site)
+		siteLen := len(site.Name)
 		if longestSiteLen < siteLen {
 			longestSiteLen = siteLen
 		}
 	}
 	for _, site := range sites {
-		spaceNum := longestSiteLen - len(site) + 1
+		spaceNum := longestSiteLen - len(site.Name) + 1
 		spaceStr := strings.Repeat(" ", spaceNum)
-		fmt.Printf("%s:%s%s\n", site, spaceStr,
-			createPassword(site, masterPassword))
+		fmt.Printf("%s:%s%s\n", site.Name, spaceStr,
+			site.Password(masterPassword))
 	}
 }
